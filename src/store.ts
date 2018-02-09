57a000cd299b7ed154953c85f3676438f5aba7aa
y0za <yoza.is12s@gmail.com>
2018-02-10 02:01:09 +0900

    Add searchResults state to store

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,37 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {
+  MutationTree,
+  ActionTree,
+  ActionContext,
+} from 'vuex';
+import {
+  TitleInfo,
+  searchTitle,
+} from './api';
 
 Vue.use(Vuex);
 
+class State {
+  public searchResults: TitleInfo[] = [];
+}
+
+const mutations = {
+  updateSearchResults(state: State, results: TitleInfo[]) {
+    state.searchResults = results;
+  },
+} as MutationTree<State>;
+
+const actions = {
+  search(context: ActionContext<State, any>, word: string) {
+    searchTitle(word).then((data) => {
+      context.commit('updateSearchResults', data);
+    });
+  },
+} as ActionTree<State, any>;
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: new State(),
+  mutations,
+  actions,
 });
