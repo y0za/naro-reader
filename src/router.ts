abd67498dd3d62acc5e1869691df836553e3dd48
y0za <yoza.is12s@gmail.com>
2018-02-11 00:26:29 +0900

    Register Title view to vue-router

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import SearchResult from './views/SearchResult.vue';
+import Title from './views/Title.vue';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/search',
       name: 'searchResult',
       component: SearchResult,
+    },
+    {
+      path: '/title/:ncode',
+      name: 'title',
+      component: Title,
     },
   ],
 });
