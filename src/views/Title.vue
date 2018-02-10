f7ca851c8944d3fc5a017db4649d716f8dbc03cc
y0za <yoza.is12s@gmail.com>
2018-02-11 00:26:09 +0900

    Add Title view

---
+++ src/views/Title.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-table
+      v-bind:data="episodes"
+      style="width: 100%">
+      <el-table-column
+        prop="title"
+        label="Episode">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Episode } from '../api';
+
+export default Vue.extend({
+  name: 'Title',
+
+  computed: {
+    episodes(): Episode[] {
+      return this.$store.state.episodes as Episode[];
+    },
+  },
+
+  created() {
+    const ncode = this.$route.params.ncode;
+    this.$store.dispatch('getEpisodes', ncode);
+  },
+});
+</script>
