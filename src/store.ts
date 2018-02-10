0aad12edc3280797efb9b4b7e378f33882293b62
y0za <yoza.is12s@gmail.com>
2018-02-11 00:19:37 +0900

    Add episodes state to store

--- src/store.ts
+++ src/store.ts
@@ -7,18 +7,24 @@ import {
 } from 'vuex';
 import {
   TitleInfo,
+  Episode,
   searchTitle,
+  fetchEpisodes,
 } from './api';
 
 Vue.use(Vuex);
 
 class State {
   public searchResults: TitleInfo[] = [];
+  public episodes: Episode[] = [];
 }
 
 const mutations = {
   updateSearchResults(state: State, results: TitleInfo[]) {
     state.searchResults = results;
+  },
+  updateEpisodes(state: State, episodes: Episode[]) {
+    state.episodes = episodes;
   },
 } as MutationTree<State>;
 
@@ -26,6 +32,11 @@ const actions = {
   search(context: ActionContext<State, any>, word: string) {
     searchTitle(word).then((data) => {
       context.commit('updateSearchResults', data);
+    });
+  },
+  getEpisodes(context: ActionContext<State, any>, ncode: string) {
+    fetchEpisodes(ncode).then((episodes) => {
+      context.commit('updateEpisodes', episodes);
     });
   },
 } as ActionTree<State, any>;
