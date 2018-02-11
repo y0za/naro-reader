e5b7d2e8ff581ce8c2a4a750265ecc3aaaf326c3
y0za <yoza.is12s@gmail.com>
2018-02-11 16:04:53 +0900

    Add row click handler to Novel view table

--- src/views/Novel.vue
+++ src/views/Novel.vue
@@ -2,6 +2,7 @@
   <div>
     <el-table
       v-bind:data="episodes"
+      v-on:row-click="showEpisode"
       style="width: 100%">
       <el-table-column
         prop="title"
@@ -19,14 +20,28 @@ export default Vue.extend({
   name: 'Novel',
 
   computed: {
+    ncode(): string {
+      return this.$route.params.ncode;
+    },
     episodes(): Episode[] {
       return this.$store.state.episodes as Episode[];
     },
   },
 
+  methods: {
+    showEpisode(episode: Episode) {
+      this.$router.push({
+        name: 'episode',
+        params: {
+          ncode: this.ncode,
+          id: episode.id,
+        },
+      });
+    },
+  },
+
   created() {
-    const ncode = this.$route.params.ncode;
-    this.$store.dispatch('getEpisodes', ncode);
+    this.$store.dispatch('getEpisodes', this.ncode);
   },
 });
 </script>
