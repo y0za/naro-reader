2d87106c0cd513e57b4fa2b25cd011e3166e602b
y0za <yoza.is12s@gmail.com>
2018-02-13 20:44:11 +0900

    Add navigation drawer and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" class="navigation-drawer" app fixed>
+      <v-list dense>
+        <router-link v-bind:to="{ name: 'home' }">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Naro Reader</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -9,5 +31,17 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'App',
+
+  data() {
+    return {
+      drawer: null,
+    };
+  },
 });
 </script>
+
+<style>
+.navigation-drawer a {
+  text-decoration: none;
+}
+</style>
