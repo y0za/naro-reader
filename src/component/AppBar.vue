267a6a4a28a500cdb3543f07c5bd6a441d62723f
y0za <yoza.is12s@gmail.com>
2018-02-22 01:33:18 +0900

    Add NovelActions component to AppBar component

--- src/component/AppBar.vue
+++ src/component/AppBar.vue
@@ -2,6 +2,13 @@
   <v-toolbar app fixed>
     <v-toolbar-side-icon v-on:click.stop="toggleDrawer"></v-toolbar-side-icon>
     <v-toolbar-title>Naro Reader</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <novel-actions
+      v-if="$route.name === 'novel'"
+      v-bind:is-bookmarked="bookmarked"
+      v-bind:toggle-bookmarked="toggleBookmarked"
+      v-bind:reset-bookmarked="resetBookmarked"
+    ></novel-actions>
   </v-toolbar>
 </template>
 
@@ -11,10 +18,30 @@ import {
   Component,
   Prop,
 } from 'vue-property-decorator';
+import { mapState, mapActions } from 'vuex';
+import NovelActions from './NovelActions.vue';
 
-@Component
+@Component({
+  components: {
+    NovelActions,
+  },
+  computed: {
+    ...mapState(['bookmarked']),
+  },
+  methods: {
+    ...mapActions(['toggleBookmarked']),
+  },
+})
 export default class AppBar extends  Vue {
   @Prop()
   public toggleDrawer!: () => void;
+
+  get isBookmarked() {
+    return this.$store.state.bookmarked;
+  }
+
+  public resetBookmarked() {
+    this.$store.commit('resetBookmarked');
+  }
 }
 </script>
