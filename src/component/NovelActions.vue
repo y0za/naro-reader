09d904e1e6129b6ee53fb142a45bdb3954e3ab5e
y0za <yoza.is12s@gmail.com>
2018-02-22 01:31:48 +0900

    Add NovelActions component

---
+++ src/component/NovelActions.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-btn 
+      icon
+      v-on:click="toggleBookmarked"
+    >
+      <v-icon
+        v-bind:color="bookmarkIconColor"
+      >star</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Vue,
+  Component,
+  Prop,
+} from 'vue-property-decorator';
+
+@Component
+export default class NovelActions extends  Vue {
+  @Prop()
+  public isBookmarked!: boolean;
+
+  @Prop()
+  public toggleBookmarked!: () => void;
+
+  @Prop()
+  public resetBookmarked!: () => void;
+
+  get bookmarkIconColor() {
+    return this.isBookmarked ? 'yellow darken-2' : 'grey';
+  }
+
+  public beforeDestroy() {
+    this.resetBookmarked();
+  }
+}
+</script>
