983b4216bc44339c1ebb92de3a891315707f3bb0
y0za <yoza.is12s@gmail.com>
2018-02-26 00:01:29 +0900

    Add proxy to devServer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,31 @@
+const webpack = require('webpack');
+
+const publicProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const naroRealUrl = 'https://ncode.syosetu.com';
+const naroBaseUrl = publicProxyUrl + naroRealUrl;
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        NARO_BASE_URL: process.env.NODE_ENV === 'production' ? naroBaseUrl : '/naro_proxy/'
+      })
+    ]
+  },
+  devServer: {
+    proxy: {
+      '/naro_proxy': {
+        target: naroRealUrl,
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/naro_proxy': ''
+        },
+        onProxyRes(proxyRes, req, res) {
+          proxyRes.headers['Access-Control-Allow-Origin'] = '*'
+        }
+      }
+    }
+  }
 }
