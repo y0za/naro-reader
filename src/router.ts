a4daf0f8fc1b4b4bf514ae93f7a003e082353a9e
y0za <yoza.is12s@gmail.com>
2018-03-26 00:15:48 +0900

    Register BookmarkedNovels to vue-router

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './view/Home.vue';
 import SearchResult from './view/SearchResult.vue';
+import BookmarkedNovels from './view/BookmarkedNovels.vue';
 import Novel from './view/Novel.vue';
 import Chapter from './view/Chapter.vue';
 
@@ -19,6 +20,11 @@ export default new Router({
       path: '/search',
       name: 'searchResult',
       component: SearchResult,
+    },
+    {
+      path: '/bookmarked-novels',
+      name: 'bookmarkedNovels',
+      component: BookmarkedNovels,
     },
     {
       path: '/novel/:ncode',
